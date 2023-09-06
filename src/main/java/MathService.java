288b4e04011a4ac156bd31ac38f1e282de0b0d4b
kravmaxim@gmail.com <Cegthgfhjkm!666>
2023-09-06 20:10:15 +0300

    -

--- src/main/java/MathService.java
+++ src/main/java/MathService.java
@@ -1,7 +1,7 @@
 public class MathService {
 
     public Pair getAnswer(int a, int b, int c) throws NotFoundAnswerException {
-        int d = b * b - 4 * a * c;
+        int d = getD(a,b,c);
         if (d < 0) {
             throw new NotFoundAnswerException("Корни не могут быть найдены");
         }
@@ -15,5 +15,9 @@ public class MathService {
             return new Pair(x1, x2);
         }
         return null;
+    }
+
+    public int getD(int a, int b, int c) {
+        return b * b - 4 * a * c;
     }
 }
